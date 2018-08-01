ec20ac347af866f13ca75dfad3d74b43fca9c661
Viktor Korolev <vik_kor@softgrad.com>
2018-08-01 13:31:27 +0300

    Состояние компонентов Task 1 Портфолио с фильтрами

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,37 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'activeFilter': 'all'
+    }
+  }
+
+  sort(projects, category) {
+    if (category.toLowerCase() === 'all')
+      return projects;
+
+    return projects.filter(function (el) {
+      return el.category === category;
+    })
+  }
+
+  render() {
+    const {filters, projects} = this.props;
+    return (
+      <div>
+        <Toolbar
+          filters={filters}
+          selected={this.state.activeFilter}
+          onSelectFilter={(filter) => {
+            this.setState({
+              'activeFilter': filter
+            });
+            console.log(filter);
+          }}/>
+        <Portfolio projects={this.sort(projects, this.state.activeFilter)}/>
+      </div>
+    );
+  }
+}
