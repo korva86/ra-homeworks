ba046a735dad0cd5cbe7d74c561cdf7f43f4bf87
Viktor Korolev <vik_kor@softgrad.com>
2018-08-01 13:07:52 +0300

    Обработка событий Task 3 Форма авторизации

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,41 @@
-'use strict';
+//'use strict';
+function AuthForm (props) {
+  let userName, email, pass;
+  const changePass = function(e) {
+     e.target.value = e.target.value.replace(/\W/,'');
+   };
+  const changeEmail = function(e) {
+     e.target.value = e.target.value.replace(/[^A-Za-z0-9_@.-]/,'');
+   };
+  const Submit = function(e) {
+     e.preventDefault();
+     let formData = {
+       "name": userName.value,
+       "email": email.value,
+       "password": pass.value
+     };
+     console.log(formData);
+     if(props.onAuth || typeof props.onAuth == 'function') {props.onAuth(formData)};
+     
+   }
+   return (
+     <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={Submit}>
+       <div className="Input">
+         <input required type="text" placeholder="Имя" ref={element => userName = element} />
+         <label></label>
+       </div>
+       <div className="Input">
+         <input type="email" placeholder="Электронная почта" onChange={changeEmail} ref={element => email = element} />
+         <label></label>
+       </div>
+       <div className="Input">
+         <input required type="password" placeholder="Пароль" onChange={changePass} ref={element => pass = element} />
+         <label></label>
+       </div>
+       <button type="submit">
+         <span>Войти</span>
+         <i className="fa fa-fw fa-chevron-right"></i>
+       </button>
+     </form>
+   )
+ };
