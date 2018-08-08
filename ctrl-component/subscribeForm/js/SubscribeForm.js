0291539f6184ed7b854333011849af6633313fc5
Viktor Korolev <vik_kor@softgrad.com>
2018-08-08 16:37:47 +0300

    Контролируемые компоненты Task 1 Форма подписки

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,5 +1,43 @@
-const SubscribeForm = () => {
-    return (
-        <div>Поиск</div>
-    )
-};
+'use strict';
+
+class SubscribeForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        email: '',
+        valid: ''
+      }
+    }
+    
+    render() {
+        return (
+            <div className="subscribe__form">
+                <form className={"form form--subscribe " + this.state.valid}>
+                    <h4 className="form-title">Подписаться:</h4>
+                    <div className="form-group">
+                        <label htmlFor="input-email" className="sr-only">Email</label>
+                        <input 
+                            className="form-control"
+                            type="email" 
+                            id="input-email" 
+                            placeholder="Email"
+                            value={this.state.email}
+                            onChange={this.changeEmail.bind(this)}
+                        />
+                        <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
+                        <button type="submit" className="form-next">
+                            <i className="material-icons">keyboard_arrow_right</i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+    changeEmail(event) { 
+        this.setState({ email: event.currentTarget.value }); 
+        if (event.currentTarget.value == "") {
+            this.setState({ valid: 'is-error' });
+        } else if (event.currentTarget.validity.valid) {this.setState({ valid: 'is-valid' });}
+        else {this.setState({ valid: 'is-error' });}
+    }
+}
