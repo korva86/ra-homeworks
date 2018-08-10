7264c65019491b7467cbf2b6eb26402a22e4b555
Viktor Korolev <vik_kor@softgrad.com>
2018-08-10 13:23:26 +0300

    Контролируемые компоненты Task 3 Конструктор футболки

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,18 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
+const FontSelector = ({fonts, onSelect, selected}) => {
     return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
-    )
+    <div className="font-picker">  
+        {fonts.map(function (font, i) { 
+        return (
+        <div className="grid center font-item">
+            <input type="radio" name="font" onChange={evt => onSelect(font)} value={"abc" + (i+1)} id={"abc" + (i+1)} checked={font === selected}/>
+            <label htmlFor={"abc" + (i+1)} className={"grid-" + (i+1)}>
+                <PictureFont
+                    text="abc"
+                    path={font.path}
+                />
+            </label>
+        </div>)}
+        )}
+    </div>
+    );
 };
--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,19 @@
 const TextRenderLine = ({value, onChange}) => {
+	const handleChange = function (func, val) {
+		val = val.replace(/[^A-z\s]/g, '');
+		func(val);
+	}
 	return (
 		<div className="type-text">
-			Наберите текст
+			<textarea 
+				name="text" 
+				id="font-text" 
+				cols="30" rows="2" 
+				placeholder="Введите текст для футболки"
+				value={value}
+				onChange={(e) => {handleChange(onChange, e.currentTarget.value)} }
+				>
+			</textarea>
 		</div>
 	);
 };
