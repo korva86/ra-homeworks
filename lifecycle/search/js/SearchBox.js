22a661d7f2b710e9dd8e1fd38c0f77ebdfff988f
Viktor Korolev <vik_kor@softgrad.com>
2018-08-13 20:12:19 +0300

    Жизненный цикл компонентов Task 1 Плавающая поисковая строка

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -5,15 +5,40 @@ class SearchBox extends React.Component {
     this.state = { fixed: false };
   }
 
+  componentDidMount () {
+    let searchBox = document.querySelector('.search-box').closest(".col-sm-12");
+    const searchBoxTop = searchBox.offsetTop;
+    this.scrollHandler = this.scrollHandler.bind(this);
+    this.setPosition = this.setPosition.bind(this);
+    document.addEventListener(
+      'scroll',
+      this.scrollHandler
+    );
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(
+      'scroll',
+      this.scrollHandler
+    )
+  }
+
   render() {
     return <SearchBoxView fixed={this.state.fixed} />
   }
 
-  isFixed() {
-    return undefined;
+  isFixed(searchBoxTop) {
+    let h = window.pageYOffset;
+    return h >= searchBoxTop;
   }
 
   setPosition() {
-    return undefined;
+    let searchBox = document.querySelector('.search-box').closest(".col-sm-12");
+    const searchBoxTop = searchBox.offsetTop;
+    this.setState({fixed: this.isFixed(searchBoxTop)});
+  }
+
+  scrollHandler () {
+    this.setPosition()
   }
 }
