bb4b050b4f8f747271ceb5136715e041e82b10a9
Viktor Korolev <vik_kor@softgrad.com>
2018-09-04 17:48:43 +0300

    Жизненный цикл компонентов Task 3 Корзина

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -2,7 +2,6 @@ class Cart extends React.Component {
 
   shouldComponentUpdate(nextProps) {
     const {isOpen, items} = nextProps;
-    console.log(items);
     return this.props.isOpen !== isOpen || (this.props.isOpen && items.length !== this.props.items);
   }
 
