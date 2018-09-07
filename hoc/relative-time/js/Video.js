8980b84a18ee16d53b9df8cce7697d3776c9a7c4
Viktor Korolev <vik_kor@softgrad.com>
2018-09-07 19:41:08 +0300

    Компоненты высшего порядка Task1 Форматирование даты публикации

--- hoc/relative-time/js/Video.js
+++ hoc/relative-time/js/Video.js
@@ -4,7 +4,42 @@ const Video = props => {
     return (
         <div className="video">
             <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTime date={props.date} />
+            <PrettyDateTime date={props.date} />
         </div>
     )
 };
+
+
+const prettyDate = (Component) => {
+    return class prettifyDate extends React.Component {
+
+        getDateDiff(dateFromData, DateTo = new Date()) {
+            const DateFrom = dateFromData instanceof Date ? dateFromData : new Date(dateFromData);
+            const DATEDIFF = (DateTo.getTime() - DateFrom.getTime());
+
+            return {
+                days: Math.floor(DATEDIFF / (24 * 60 * 60 * 1000)),
+                hours: Math.floor(DATEDIFF / (60 * 60 * 1000)),
+                minutes: Math.floor(DATEDIFF / (60 * 1000))
+            };
+        }
+
+        prettyDateFunc(diffTime) {
+            if (diffTime.days) {
+                return `${diffTime.days} дней назад`;
+            } else if (diffTime.hours) {
+                return `${diffTime.hours} часов назад`;
+            } else if (diffTime.minutes) {
+                return `${diffTime.minutes} минут назад`;
+            } else return null;
+        } 
+
+        render () {
+            const TIMEDIFF = this.getDateDiff(this.props.date);
+            const PRETTYDATE = this.prettyDateFunc(TIMEDIFF)
+            return <Component {...this.props} date={PRETTYDATE} />
+        }
+    }
+}
+
+const PrettyDateTime = prettyDate(DateTime);
