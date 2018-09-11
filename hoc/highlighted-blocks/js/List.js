c15c4179b95f166b72cbd66ea90f0f3f9c904483
Viktor Korolev <vik_kor@softgrad.com>
2018-09-11 16:06:46 +0300

    Компоненты высшего порядка Task2 Подсвечивание блоков

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,16 +1,39 @@
 'use strict';
+const NewComponent = (Component) => 
+  class extends React.Component {
+    render () {
+    const {views} = this.props;
+    if (views < 100) {
+      return (
+      <New>
+        <Component {...this.props} />
+      </New>
+      )
+    } else if (views > 1000) {
+      return (
+      <Popular>
+        <Component {...this.props} />
+      </Popular>
+      )
+    } else
+      return <Component {...this.props} />
+    }
+  }
+
+const ArticleComponent = NewComponent(Article);
+const VideoComponent = NewComponent(Video);
 
 const List = props => {
     return props.list.map(item => {
         switch (item.type) {
             case 'video':
                 return (
-                    <Video {...item} />
+                    <VideoComponent {...item} />
                 );
 
             case 'article':
                 return (
-                    <Article {...item} />
+                    <ArticleComponent {...item} />
                 );
         }
     });
